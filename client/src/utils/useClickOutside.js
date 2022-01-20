a5b3509d1efdb0092f5122b2f24bcf72df737142
Nguyen Minh Khanh <nmkhanh00@gmail.com>
2022-01-20 21:26:04 +0700

    feat: add useClickOutside hook

---
+++ client/src/utils/useClickOutside.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+function useClickOutside(ref, handler) {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (!ref.current?.contains(event.target)) {
+        handler();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, handler]);
+}
+
+export default useClickOutside;
