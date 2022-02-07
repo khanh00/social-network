9394abbb49ae54e1bd528ea288cb5bc95f23b8ff
Nguyen Minh Khanh <nmkhanh00@gmail.com>
2022-02-07 21:31:07 +0700

    refactor: rename folder to tokenUtil

--- server/src/utils/index.js
+++ server/src/utils/index.js
@@ -1,5 +1,5 @@
 export { default as AppError } from './AppError';
-export { default as authUtil } from './auth';
-export { default as catchAsync } from './catchAsync';
+export { default as tokenUtil } from './tokenUtil';
 export { default as customValidation } from './customValidation';
+export { default as catchAsync } from './catchAsync';
 export { default as sendJsonRes } from './sendJsonRes';
