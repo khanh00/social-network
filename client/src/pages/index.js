04a55808c8921f858ce465e1ab8a06fb1d480d71
Nguyen Minh Khanh <nmkhanh00@gmail.com>
2022-02-14 15:24:41 +0700

    feat: initialize pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Error } from './Error';
+export { default as Friends } from './Friends';
+export { default as Groups } from './Groups';
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Messages } from './Messages';
+export { default as Profile } from './Profile';
+export { default as Settings } from './Settings';
