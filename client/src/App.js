45861102c366b48a5f80254cd6790e6f7f525eb0
Nguyen Minh Khanh <nmkhanh00@gmail.com>
2022-02-14 15:24:41 +0700

    feat: initialize pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import RequireAuth from './components/RequireAuth';
-import { Home, Login } from './pages';
+import {
+  Error,
+  Friends,
+  Groups,
+  Home,
+  Login,
+  Messages,
+  Profile,
+  Settings,
+} from './pages';
 import { AuthProvider } from './contexts/authContext';
+import RequireAuth from './components/RequireAuth';
 import { SocketProvider } from './contexts/socketContext';
 
 function App() {
@@ -13,9 +22,16 @@ function App() {
         <Router>
           <Routes>
             <Route path="/login" element={<Login />} />
+            {/* <Route path="/register" element={<Register />} /> */}
+            <Route path="/error" element={<Error />} />
 
             <Route element={<RequireAuth />}>
               <Route path="/" element={<Home />} />
+              <Route path="/friends" element={<Friends />} />
+              <Route path="/groups" element={<Groups />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/profiles/:id" element={<Profile />} />
             </Route>
           </Routes>
         </Router>
---
+++ client/src/pages/Friends/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Friends() {
+  return <h1>Friend</h1>;
+}
+
+export default Friends;
